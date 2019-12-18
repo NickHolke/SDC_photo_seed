0f0d18feff055df164544fc2b580bd356411e86a
Nick Holke <nickholke@gmail.com>
2019-12-18 12:30:34 -0800

    Downloaded photos and posted to S3

--- image_download.js
+++ image_download.js
@@ -1,14 +1,22 @@
 const download = require('image-downloader');
 const { exec } = require('child_process');
 
+const rooms = ['bedroom', 'door', 'backyard', 'diningroom', 'kitchen', 'foyer', 'patio', 'backyard', 'porch', 'hallway']
+let index = -1;
+
 for (let i = 1; i < 1001; i++) {
-  if (i % 100 === 0) {
-    console.log(`${i} images saved`)
+  index++;
+  let room = rooms[index];
+
+  if (index === 10) {
+    index = -1;
   }
+
   let options = {
-    url: 'https://source.unsplash.com/random/800x600/?room',
+    url: `https://source.unsplash.com/random/800x600/?${room}`,
     dest: `./images/photo${i}.jpg`              
   }
+
   download.image(options)
     .then(({ filename, image }) => {
       console.log('Saved to ', filename);
@@ -16,12 +24,4 @@ for (let i = 1; i < 1001; i++) {
     .catch((err) => console.log(err));
 }
 
-exec('aws s3 sync ./images s3://sdcroomphotos/photos', (err, stdout, stderr) => {
-  if (err) {
-    //some err occurred
-    console.error(err)
-  } else {
-   // the *entire* stdout and stderr (buffered)
-   console.log('images uploaded successfully')
-  }
-});
+
