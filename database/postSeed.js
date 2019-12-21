423b7bb88c08c72b270153febd1eadc76e45b425
Nick Holke <nickholke@gmail.com>
2019-12-20 19:17:48 -0800

    Successfully seeded 10 million listings

--- database/postSeed.js
+++ database/postSeed.js
@@ -1,5 +1,6 @@
 const faker = require('faker');
 const fs = require('fs');
+const start = Date.now();
 
 const { Pool, Client } = require('pg');
 
@@ -10,68 +11,35 @@ const pool = new Pool({
   port: '5432'
 })
 
-// const text = 'INSERT INTO listings(title, hostname, address, city, state) VALUES($1, $2, $3, $4, $5)'
-// let i = 10; 
-
-// while (i > 0) {
-//   let values = [
-//      faker.lorem.words(),
-//      faker.name.firstName(),
-//      faker.address.streetAddress(),
-//      faker.address.city(),
-//      faker.address.state()
-//     ]
-//   pool.query(text, values, (err, res) => {
-//     if (err) {
-//       console.log(err.stack)
-//     } else {
-//       console.log('inserted row')
-//     }
-    
-//   })
-
-//   pool.query(text, values)
-//       .then(res => console.log('row inserted'))
-//       .catch(e => console.log(e));
-
-//   i--;
-// }  
-
-// pool.end()
-
-
-
 const createStr = () => {
   const createRow = () => {
-    return `${faker.lorem.words()},${faker.name.firstName()},${faker.address.streetAddress()},${faker.address.city()},${faker.address.state()}\n`
+    return `${faker.lorem.words()},${faker.name.firstName()},${faker.address.streetAddress()},${faker.address.city()},${faker.address.stateAbbr()}\n`
   }
   let csvString = 'title,hostname,address,city,state\n';
-  let rows = 100;
+  let rows = 1000000;
 
   while (rows > 0) {
     csvString += createRow();
     rows--;
   }
-
   return csvString
 }
 
-fs.writeFile('./listings.csv', createStr(), (err) => {
-  if (err) {
-    console.log(err);
-  }
-  pool.query(`COPY listings(title,hostname,address,city,state) FROM '/Users/nickholke/Desktop/HackReactor/SDC/photo-download/listings.csv' DELIMITER ',' CSV HEADER`, (err, res) => {
-    if (err) {
+async function copyCSV () {
+  for (let i = 0; i < 10; i++) {
+    fs.writeFileSync('./listings.csv', createStr());
+    try {
+      await pool.query(`COPY listings(title,hostname,address,city,state) FROM '/Users/nickholke/Desktop/HackReactor/SDC/photo-download/listings.csv' DELIMITER ',' CSV HEADER`)
+      console.log('csv inserted');
+      console.log((Date.now() - start) / 1000);
+    } catch (err) {
       console.log(err)
-    } else {
-      console.log('inserted csv')
     }
-  })
-  pool.end()
-})
+  }
 
+  pool.end();
+}
 
-
-
+copyCSV();
 
 
